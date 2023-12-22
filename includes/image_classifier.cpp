37386ea6502aad076117de2dad98945ee7f9d0ad
JoenTheWizard <joenwiz@localhost.localdomain>
2023-12-22 13:27:33 +1100

    update the image classifier class, will make getters and setters for the MLP_NN struct later

--- includes/image_classifier.cpp
+++ includes/image_classifier.cpp
@@ -5,7 +5,115 @@ extern "C" {
 }
 
 ImageClassifier::ImageClassifier() {
-    
+    //To initialize the neural network struct
+    hidden_layer_nodes = new unsigned int[1];
+    hidden_layer_nodes[0] = 200;
+    neural_network = {
+        .num_inputs = 784,
+        .num_outputs = 2,
+        .learning_rate = 0.05,
+        .epoch = 10000,
+        //Initialize neurons and weights to NULL
+        .neurons = NULL, .weights = NULL
+    };
+    neural_network.num_hidden = hidden_layer_nodes;
+
+    num_of_hidden_layers = 1; //sizeof(hidden_layer_nodes) / sizeof(hidden_layer_nodes[0]);
+}
+
+int ImageClassifier::train_from_dataset(const char* dataset) {
+    Matrix input_nodes;
+    Matrix output_nodes;
+
+    printf("No Input: %i No Output: %i\n", neural_network.num_inputs, neural_network.num_outputs);
+    printf("Learning Rate: %f Epoch: %i\n", neural_network.learning_rate, neural_network.epoch);
+    printf("Hidden Layers:\n");
+    for (int i = 0; i < num_of_hidden_layers; i++) {
+        printf("= %d =\n", neural_network.num_hidden[i]);
+    }
+
+    if (!read_dataset(dataset, neural_network.num_inputs, neural_network.num_outputs, &input_nodes, &output_nodes)) {
+        printf("Error reading the data set (check if file exists)\n");
+        free_matrix(&input_nodes);
+        free_matrix(&output_nodes);
+        return -1;
+    } else {
+        Matrix* inputs = get_row_matrix(&input_nodes, 0);
+
+        size_t num_weight_layers = initialize_rand_weights(&neural_network, num_of_hidden_layers);
+
+        init_mlp_model(&neural_network, inputs, num_weight_layers);
+
+        train_mlp_model(&neural_network, &input_nodes, &output_nodes, num_weight_layers);
+
+        free_matrix(inputs);
+        free(inputs);
+        free_matrix(&input_nodes);
+        free_matrix(&output_nodes);
+    }
+
+    return 0;
+}
+
+int ImageClassifier::train_from_dataset_load_weights(const char* dataset, const char* weightsFile) {
+    Matrix input_nodes;
+    Matrix output_nodes;
+
+    printf("No Input: %i No Output: %i\n", neural_network.num_inputs, neural_network.num_outputs);
+    printf("Learning Rate: %f Epoch: %i\n", neural_network.learning_rate, neural_network.epoch);
+    printf("Hidden Layers:\n");
+    for (int i = 0; i < num_of_hidden_layers; i++) {
+        printf("= %d =\n", neural_network.num_hidden[i]);
+    }
+
+    if (!read_dataset(dataset, neural_network.num_inputs, neural_network.num_outputs, &input_nodes, &output_nodes)) {
+        printf("Error reading the data set (check if file exists)\n");
+        free_matrix(&input_nodes);
+        free_matrix(&output_nodes);
+        return -1;
+    } else {
+        Matrix* inputs = get_row_matrix(&input_nodes, 0);
+
+        size_t num_weight_layers = load_mlp_weights(&neural_network, weightsFile, num_of_hidden_layers);
+
+        init_mlp_model(&neural_network, inputs, num_weight_layers);
+
+        train_mlp_model(&neural_network, &input_nodes, &output_nodes, num_weight_layers);
+
+        free_matrix(inputs);
+        free(inputs);
+        free_matrix(&input_nodes);
+        free_matrix(&output_nodes);
+    }
+
+    return 0;
+}
+
+int ImageClassifier::save_weights(const char* weightsFile) {
+    if (neural_network.weights != NULL) {
+        size_t num_weight_layers = num_of_hidden_layers + 1;
+        save_mlp_weights(&neural_network, weightsFile, num_weight_layers);
+        return 0;
+    }
+    return -1;
+}
+
+int ImageClassifier::load_weights(const char* weightsFile) {
+    size_t num_weight_layers = load_mlp_weights(&neural_network, weightsFile, num_of_hidden_layers);
+    return 0;
+}
+
+void ImageClassifier::forward_propagate_img(const char* imagePath) {
+    Matrix inputs;
+    flatten_img_data(imagePath, &inputs);
+    size_t num_weight_layers = num_of_hidden_layers + 1;
+    if (neural_network.neurons == NULL)
+        init_mlp_model(&neural_network, &inputs, num_weight_layers);
+
+    if (neural_network.neurons != NULL)
+        forward_propagate(&neural_network, &inputs, num_weight_layers);
+
+    free_matrix(&inputs);
 }
 
 //Flatten the image data from the selected colour channel
@@ -14,7 +122,7 @@ void ImageClassifier::flatten_img_data(const char* filePath, Matrix* result, Col
     int x,y,n;
     unsigned char* imgData = stbi_load(filePath, &x, &y, &n, 0);
 
-    init_matrix(result, y, x);
+    init_matrix(result, 1, x*y);
 
     if (imgData != nullptr && x > 0 && y > 0) {
         //RGB channels
@@ -25,8 +133,20 @@ void ImageClassifier::flatten_img_data(const char* filePath, Matrix* result, Col
                 double r_scaled = r / 255.0f;
 
                 //Assign the values grayscaled values to the matrix
-                result->data[i / y][i % y] = r_scaled; 
+                result->data[0][i] = r_scaled; 
             }
         }
     }
+    stbi_image_free(imgData);
+}
+
+ImageClassifier::~ImageClassifier() {
+    printf("Dellaocating\n");
+    size_t num_weight_layers = num_of_hidden_layers + 1;
+    if (neural_network.weights != NULL)
+        free_mat_array(&neural_network.weights, num_weight_layers);
+    if (neural_network.neurons != NULL)
+        free_mat_array(&neural_network.neurons, num_weight_layers + 1);
+    
+    delete[] hidden_layer_nodes;
 }
--- includes/image_classifier.hpp
+++ includes/image_classifier.hpp
@@ -11,10 +11,25 @@ enum ColourChannel { RED = 0, GREEN = 1, BLUE = 2 };
 //This will be used for the Image classification utilizing the MLP neural network
 class ImageClassifier {
 public:
+    //Will initialize the neural network with default parameters
     ImageClassifier();
+    ~ImageClassifier();
     MLP_NN neural_network;
-    //Will flatten the grayscaled image by reading the specified colour channel values
-    static void flatten_img_data(const char* filePath, Matrix* result, ColourChannel channel);
+    //Read the specified dataset and train
+    int train_from_dataset(const char* dataset);
+    int train_from_dataset_load_weights(const char* dataset, const char* weightsFile);
+    //Load and save weights
+    int save_weights(const char* weightsFile);
+    int load_weights(const char* weightsFile);
+    //Forward propagate
+    void forward_propagate_img(const char* imagePath);
+    //Will flatten the grayscaled image by reading the specified colour channel values and return Matrix
+    static void flatten_img_data(const char* filePath, Matrix* result, ColourChannel channel = RED);
+private:
+    //We need to know the size of the network (total_layers = layer_neurons + layer_weights)
+    //Num of weight layers = num of hidden layers + 1
+    size_t num_of_hidden_layers;
+    unsigned int* hidden_layer_nodes;
 };
 
 #endif
